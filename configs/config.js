e306e03200adca19b0e0dce08ca2373a834448d6
Lê Trường Thịnh <thinhle.250902@gmail.com>
2024-04-02 21:18:05 +0700

    init bai tap

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SECRET_KEY: 'TUNG123',
+    EXPIRE_JWT: '1d',
+    hostName: 'localhost:3000',
+    Host: 'sandbox.smtp.mailtrap.io',
+    Port: '2525',
+    Username: '07cca08efc139a',
+    Password: 'fef29e3fc163a9'
+}
